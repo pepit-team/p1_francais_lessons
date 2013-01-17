2bdd1008fc14e78694757d1f96fc822d8a654450
Eric Ramat <eramat@users.sourceforge.net>
2013-01-17 17:40:16 +0100

    add first version of code without images

---
+++ src/org/pepit/p1/francais/lessons/Response.java
@@ -0,0 +1,28 @@
+/**
+ * @file org/pepit/p1/francais/lessons/Response.java
+ * 
+ * PepitModel: an educational software
+ * This file is a part of the PepitModel environment
+ * http://pepit.be
+ *
+ * Copyright (C) 2012-2013 Pepit Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.pepit.p1.francais.lessons;
+
+public enum Response {
+    SEE_AND_HEAR, SEE_AND_NOT_HEAR, NOT_SEE_AND_NOT_HEAR
+}
